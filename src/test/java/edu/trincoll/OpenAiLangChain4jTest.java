afea3fe3370909508cc426dff9437767d0400f03
Ken Kousen <ken.kousen@kousenit.com>
2024-09-19 20:46:43 -0400

    Enhance ChatModel tests with message, string, and vision support
    Refactor the existing chat test to use ChatLanguageModel. Add new tests to handle input as string and image with text for vision analysis, ensuring comprehensive coverage of various use cases.

--- src/test/java/edu/trincoll/OpenAiLangChain4jTest.java
+++ src/test/java/edu/trincoll/OpenAiLangChain4jTest.java
@@ -1,10 +1,15 @@
 package edu.trincoll;
 
+import dev.langchain4j.data.message.AiMessage;
+import dev.langchain4j.data.message.ImageContent;
+import dev.langchain4j.data.message.TextContent;
 import dev.langchain4j.data.message.UserMessage;
+import dev.langchain4j.model.chat.ChatLanguageModel;
 import dev.langchain4j.model.chat.request.ChatRequest;
 import dev.langchain4j.model.chat.response.ChatResponse;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModelName;
+import dev.langchain4j.model.output.Response;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -12,13 +17,14 @@ import java.util.List;
 public class OpenAiLangChain4jTest {
     private final String apiKey = System.getenv("OPENAI_API_KEY");
 
-    @Test
-    void chat() {
-        OpenAiChatModel chatModel = OpenAiChatModel.builder()
+    private final ChatLanguageModel chatModel = OpenAiChatModel.builder()
                 .apiKey(apiKey)
                 .modelName(OpenAiChatModelName.GPT_4_O_MINI)
                 //.modelName("o1-preview")
                 .build();
+
+    @Test
+    void chatWithMessages() {
         ChatResponse response = chatModel.chat(ChatRequest.builder()
                 .messages(List.of(
                         new UserMessage("""
@@ -28,6 +34,27 @@ public class OpenAiLangChain4jTest {
                 .build());
         System.out.println(response.aiMessage().text());
         System.out.println(response.tokenUsage());
+    }
+
+    @Test
+    void chatWithString() {
+        String answer = chatModel.generate("""
+                Who is the best musician of the
+                2010s?
+                """);
+        System.out.println(answer);
+    }
+
+    @Test
+    void visionChat() {
+        String imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg";
+        Response<AiMessage> response = chatModel.generate(
+                UserMessage.from(
+                        ImageContent.from(imageUrl),
+                        TextContent.from("What do you see?")
+                )
+        );
+        System.out.println(response.content().text());
     }
 
 }
